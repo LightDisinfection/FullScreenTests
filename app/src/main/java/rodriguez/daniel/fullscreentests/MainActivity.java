407d992ec4b1c1493bdf83eeb2024cd695a89db4
Daniel Rodríguez <dani1610@hotmail.com>
2018-01-31 11:10:19 +0800

    Initial commit

---
+++ app/src/main/java/rodriguez/daniel/fullscreentests/MainActivity.java
@@ -0,0 +1,16 @@
+package rodriguez.daniel.fullscreentests;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Window;
+import android.view.WindowManager;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_main);
+    }
+}
